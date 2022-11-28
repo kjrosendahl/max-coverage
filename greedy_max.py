f96bfc2320c65c02a1db4eebb52635d56273bb99
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2022-11-28 14:27:36 -0600

    add function for running all instances

--- greedy_max.py
+++ greedy_max.py
@@ -1,13 +1,13 @@
 from parse_instance import parse_instance
 from collections import defaultdict
+import csv
 
-
-def run_greed(file_name): 
+def run_greedy(file_name): 
 
     # parse the text file for sets and elements
     # runs in O(p)
     row, sets = parse_instance(file_name) 
-    m, n, k, p = row[:]
+    k = row[2]
     k = int(k)
 
     # holds cardinality of largest set to start
@@ -31,6 +31,11 @@ def run_greed(file_name):
     Q = [] 
 
     # perform greedy algorithm: iterate through decreasing size of sets
+    # TODO: verify this works in O(p) time 
+    # runs by removing one element at a time from sets 
+    # bounded by (sum(|s|) for s in F) = O(p)
+    # see exercise 35.3-3 in textbook 
+    # TODO: ask prof if we are allowed to return less than min(m, k) if we found a solution which covers all elements (no unecessary sets)
     for size in range(max_size, 0, -1):
         if size in lengths: 
             while len(lengths[size]) != 0: 
@@ -48,8 +53,33 @@ def run_greed(file_name):
                             # remove now covered element from s_2 
                             lengths[len(s_2)].remove(s)
                             s_2.remove(e)
-                            # decrease the number of available elemenets that s_2 could cover 
+                            # decrease the number of available elements that s_2 could cover 
                             lengths[len(s_2)].add(s)
     return Q
 
-print(run_greed('instance02.txt'))
+## ---------------------------------- ## 
+
+def run_instances(num_files): 
+
+    for i in range(1,num_files+1): 
+        if i < 10: 
+            file_number = str(0) + str(i)
+        else: 
+            file_number = str(i)
+
+        # initialize input and output files 
+        input_file_name = 'instance' + file_number 
+        output_file_name = 'solution' + file_number
+        
+        # run greedy algorithm 
+        Q = run_greedy(input_file_name)
+
+        # write to solution file
+        with open(output_file_name, 'w') as csvfile: 
+            writer = csv.writer(csvfile, delimiter = ' ')
+            writer.writerow(Q)
+
+## ---------------------------------- ## 
+
+if __name__ == "__main__": 
+    run_instances(3)
